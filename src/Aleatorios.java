1a6c6c0309bab57c66de95f344fb87eb9d77dd74
HailToTheChaos <jaimedelafuente25@outlook.es>
2022-10-18 11:25:02 +0200

    Parte 2

--- src/Aleatorios.java
+++ src/Aleatorios.java
@@ -1,14 +1,28 @@
 
+import java.io.File;
+import java.util.ArrayList;
+import javax.swing.*;
+
+
 public class Aleatorios extends javax.swing.JFrame {
+
+    private ArrayList<String> historial = new ArrayList();
+    DefaultListModel modeloLista = new DefaultListModel();
 
     public Aleatorios() {
         initComponents();
+        lista_historial.setModel(modeloLista);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        dialog_historial = new javax.swing.JDialog();
+        panel_dialog = new javax.swing.JPanel();
+        etiqueta_historial = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lista_historial = new javax.swing.JList<>();
         jPanel1 = new javax.swing.JPanel();
         etiqueta_desc = new javax.swing.JLabel();
         etiqueta_n1 = new javax.swing.JLabel();
@@ -17,20 +31,59 @@ public class Aleatorios extends javax.swing.JFrame {
         spinner_n2 = new javax.swing.JSpinner();
         textField_resultado = new javax.swing.JTextField();
         boton_generar = new javax.swing.JButton();
+        checkBox_primos = new javax.swing.JCheckBox();
         jMenuBar1 = new javax.swing.JMenuBar();
         menu_fichero = new javax.swing.JMenu();
         menuItem_salir = new javax.swing.JMenuItem();
+        menuItem_guardar = new javax.swing.JMenuItem();
         menu_edicion = new javax.swing.JMenu();
         menuItem_borrar = new javax.swing.JMenuItem();
+        menuItem_historial = new javax.swing.JMenuItem();
+
+        dialog_historial.setSize(new java.awt.Dimension(175, 311));
+
+        etiqueta_historial.setText("Ultimos números generados");
+
+        jScrollPane1.setViewportView(lista_historial);
+
+        javax.swing.GroupLayout panel_dialogLayout = new javax.swing.GroupLayout(panel_dialog);
+        panel_dialog.setLayout(panel_dialogLayout);
+        panel_dialogLayout.setHorizontalGroup(
+            panel_dialogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel_dialogLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(etiqueta_historial, javax.swing.GroupLayout.DEFAULT_SIZE, 154, Short.MAX_VALUE)
+                .addContainerGap())
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+        );
+        panel_dialogLayout.setVerticalGroup(
+            panel_dialogLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(panel_dialogLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(etiqueta_historial)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 283, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout dialog_historialLayout = new javax.swing.GroupLayout(dialog_historial.getContentPane());
+        dialog_historial.getContentPane().setLayout(dialog_historialLayout);
+        dialog_historialLayout.setHorizontalGroup(
+            dialog_historialLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panel_dialog, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        dialog_historialLayout.setVerticalGroup(
+            dialog_historialLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(panel_dialog, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setResizable(false);
 
         etiqueta_desc.setText("Generador de números aleatorios en el rango indicado:");
 
-        etiqueta_n1.setText("numero 1");
+        etiqueta_n1.setText("número 1");
 
-        etiqueta_n2.setText("numero 2");
+        etiqueta_n2.setText("número 2");
 
         spinner_n2.setValue(10);
 
@@ -42,6 +95,8 @@ public class Aleatorios extends javax.swing.JFrame {
                 boton_generarActionPerformed(evt);
             }
         });
+
+        checkBox_primos.setText("Números primos");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -56,19 +111,24 @@ public class Aleatorios extends javax.swing.JFrame {
                         .addGap(125, 125, 125)
                         .addComponent(textField_resultado, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGap(133, 133, 133)
+                        .addGap(67, 67, 67)
                         .addComponent(boton_generar)))
                 .addContainerGap(24, Short.MAX_VALUE))
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addGap(0, 0, Short.MAX_VALUE)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(etiqueta_n2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(etiqueta_n1, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(spinner_n1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(spinner_n2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(86, 86, 86))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(etiqueta_n2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(etiqueta_n1, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(spinner_n1, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(spinner_n2, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(86, 86, 86))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(checkBox_primos, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(47, 47, 47))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -86,8 +146,10 @@ public class Aleatorios extends javax.swing.JFrame {
                 .addGap(33, 33, 33)
                 .addComponent(textField_resultado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(boton_generar)
-                .addContainerGap(42, Short.MAX_VALUE))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(boton_generar)
+                    .addComponent(checkBox_primos))
+                .addContainerGap(43, Short.MAX_VALUE))
         );
 
         menu_fichero.setText("Fichero");
@@ -100,17 +162,35 @@ public class Aleatorios extends javax.swing.JFrame {
         });
         menu_fichero.add(menuItem_salir);
 
+        menuItem_guardar.setText("Guardar");
+        menuItem_guardar.setEnabled(false);
+        menuItem_guardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuItem_guardarActionPerformed(evt);
+            }
+        });
+        menu_fichero.add(menuItem_guardar);
+
         jMenuBar1.add(menu_fichero);
 
         menu_edicion.setText("Edición");
 
         menuItem_borrar.setText("Borrar");
+        menuItem_borrar.setEnabled(false);
         menuItem_borrar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 menuItem_borrarActionPerformed(evt);
             }
         });
         menu_edicion.add(menuItem_borrar);
+
+        menuItem_historial.setText("Historial");
+        menuItem_historial.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                menuItem_historialActionPerformed(evt);
+            }
+        });
+        menu_edicion.add(menuItem_historial);
 
         jMenuBar1.add(menu_edicion);
 
@@ -132,8 +212,14 @@ public class Aleatorios extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void boton_generarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_boton_generarActionPerformed
-        int numeroRandom = Metodos.generarNumero(Integer.parseInt(spinner_n1.getValue().toString()), Integer.parseInt(spinner_n2.getValue().toString()));
-        textField_resultado.setText(Integer.toString(numeroRandom));
+        int n1 = Integer.parseInt(spinner_n1.getValue().toString());
+        int n2 = Integer.parseInt(spinner_n2.getValue().toString());
+        String nRandom = Integer.toString(numeroRandom(n1, n2));
+
+        historial.add(nRandom);
+        textField_resultado.setText(nRandom);
+        menuItem_guardar.setEnabled(true);
+        menuItem_borrar.setEnabled(true);
     }//GEN-LAST:event_boton_generarActionPerformed
 
     private void menuItem_salirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuItem_salirActionPerformed
@@ -144,7 +230,57 @@ public class Aleatorios extends javax.swing.JFrame {
         spinner_n1.setValue(0);
         spinner_n2.setValue(10);
         textField_resultado.setText(null);
+        menuItem_borrar.setEnabled(false);
+        historial.clear();
+        modeloLista.clear();
     }//GEN-LAST:event_menuItem_borrarActionPerformed
+
+    private void menuItem_guardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuItem_guardarActionPerformed
+        JFileChooser fchooser = new JFileChooser();
+        fchooser.setVisible(true);
+        int seleccion = fchooser.showOpenDialog(fchooser);
+        boolean append = false;
+
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            File fichero = new File(fchooser.getSelectedFile().getPath() + ".txt");
+            if (fichero.exists()) {
+                int dialogButton = JOptionPane.showConfirmDialog(null, "El fichero ya existe,¿desea sobreescribirlo?", "WARNING", JOptionPane.YES_NO_OPTION);
+                if (dialogButton == JOptionPane.NO_OPTION) {
+                    append = true;
+                }
+            }
+            Metodos.guardarEnFichero(historial, fichero, append);
+        }
+        menuItem_guardar.setEnabled(false);
+    }//GEN-LAST:event_menuItem_guardarActionPerformed
+
+    private void menuItem_historialActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuItem_historialActionPerformed
+        dialog_historial.setLocationRelativeTo(null);
+        dialog_historial.setVisible(true);
+        modeloLista.clear();
+        
+        for (int i = 0; i < historial.size(); i++) {
+            modeloLista.addElement(historial.get(i));
+        }
+    }//GEN-LAST:event_menuItem_historialActionPerformed
+
+    private int numeroRandom(int min, int max) {
+        int numeroRandom = Metodos.generarNumero(min, max);
+        int contExit = 0;
+        if (checkBox_primos.isSelected()) {
+            while (!(Metodos.esPrimo(numeroRandom) || contExit == max)) {
+                contExit++;
+                numeroRandom = Metodos.generarNumero(min, max);
+            }
+        }
+
+        if (contExit == max) {
+            JOptionPane.showMessageDialog(null, "No existen números primos entre los dos valores.", "Error", JOptionPane.WARNING_MESSAGE);
+            return 0;
+        }
+
+        return numeroRandom;
+    }
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -180,15 +316,23 @@ public class Aleatorios extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton boton_generar;
+    private javax.swing.JCheckBox checkBox_primos;
+    private javax.swing.JDialog dialog_historial;
     private javax.swing.JLabel etiqueta_desc;
+    private javax.swing.JLabel etiqueta_historial;
     private javax.swing.JLabel etiqueta_n1;
     private javax.swing.JLabel etiqueta_n2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JList<String> lista_historial;
     private javax.swing.JMenuItem menuItem_borrar;
+    private javax.swing.JMenuItem menuItem_guardar;
+    private javax.swing.JMenuItem menuItem_historial;
     private javax.swing.JMenuItem menuItem_salir;
     private javax.swing.JMenu menu_edicion;
     private javax.swing.JMenu menu_fichero;
+    private javax.swing.JPanel panel_dialog;
     private javax.swing.JSpinner spinner_n1;
     private javax.swing.JSpinner spinner_n2;
     private javax.swing.JTextField textField_resultado;
--- src/Metodos.java
+++ src/Metodos.java
@@ -1,7 +1,65 @@
 
+import java.io.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Metodos {
 
     public static int generarNumero(int min, int max) {
         return (int) (Math.random() * (max - min) + min);
+    }
+
+    public static boolean esPrimo(int numero) {
+        if (numero == 0 || numero == 1) {
+            return false;
+        }
+        for (int x = 2; x < numero / 2; x++) {
+
+            if (numero % x == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void guardarEnFichero(ArrayList<String> entrada, File fichero, boolean append) {
+
+        try {
+            fichero.createNewFile();
+            FileWriter file = new FileWriter(fichero, append);
+
+            for (int i = 0; i < entrada.size(); i++) {
+                file.write(entrada.get(i));
+                file.write("\n");
+            }
+
+            if (file != null) {
+                file.close();
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static ArrayList<String> leerFichero(File fichero) {
+        ArrayList<String> historial = new ArrayList();
+
+        try {
+            Scanner sc = new Scanner(fichero);
+
+            while (sc.hasNext()) {
+                historial.add(sc.nextLine());
+            }
+            sc.close();
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return historial;
     }
 }
