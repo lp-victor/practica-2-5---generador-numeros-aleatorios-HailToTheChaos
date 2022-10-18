a7b2f89d1ef856445fa656d36a6c7f13a0e438a1
HailToTheChaos <jaimedelafuente25@outlook.es>
2022-10-18 15:35:05 +0200

    Mejora look and feel

--- src/Aleatorios.java
+++ src/Aleatorios.java
@@ -291,7 +291,7 @@ public class Aleatorios extends javax.swing.JFrame {
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    javax.swing.UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
                     break;
                 }
             }
